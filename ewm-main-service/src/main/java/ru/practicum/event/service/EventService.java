06213a96500131a105ffd3f67b9de77a3b458797
Barkov <venediktkaverin@gmail.com>
2026-07-05 07:45:39 +0300

    Исправить checkstyle

--- ewm-main-service/src/main/java/ru/practicum/event/service/EventService.java
+++ ewm-main-service/src/main/java/ru/practicum/event/service/EventService.java
@@ -9,17 +9,22 @@ import java.util.List;
 
 public interface EventService {
     EventFullDto createEvent(Long userId, NewEventDto dto);
+
     List<EventShortDto> getUserEvents(Long userId, int from, int size);
+
     EventFullDto getUserEventById(Long userId, Long eventId);
+
     EventFullDto updateUserEvent(Long userId, Long eventId, UpdateEventUserRequest request);
 
     List<EventFullDto> getAdminEvents(List<Long> users, List<State> states, List<Long> categories,
                                       LocalDateTime rangeStart, LocalDateTime rangeEnd, int from, int size);
+
     EventFullDto updateAdminEvent(Long eventId, UpdateEventAdminRequest request);
 
     List<EventShortDto> getPublicEvents(String text, List<Long> categories, Boolean paid,
                                         LocalDateTime rangeStart, LocalDateTime rangeEnd,
                                         Boolean onlyAvailable, String sort, int from, int size,
                                         HttpServletRequest request);
+
     EventFullDto getPublicEventById(Long eventId, HttpServletRequest request);
 }
--- ewm-main-service/src/main/java/ru/practicum/request/service/ParticipationRequestService.java
+++ ewm-main-service/src/main/java/ru/practicum/request/service/ParticipationRequestService.java
@@ -8,9 +8,12 @@ import java.util.List;
 
 public interface ParticipationRequestService {
     ParticipationRequestDto createRequest(Long userId, Long eventId);
+
     List<ParticipationRequestDto> getUserRequests(Long userId);
+
     ParticipationRequestDto cancelRequest(Long userId, Long requestId);
 
     List<ParticipationRequestDto> getEventRequests(Long userId, Long eventId);
+
     EventRequestStatusUpdateResult updateRequestStatus(Long userId, Long eventId, EventRequestStatusUpdateRequest request);
 }
